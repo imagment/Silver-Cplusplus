06a5a67ad8fffe0aee3a0834c5c7feea3cec84e6
Imagment <imagment@proton.me>
2024-11-29 08:08:10 +0900

    Add files via upload

---
+++ build/CMakeFiles/Silver.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Silver.dir/src/game.cpp.o"
+  "CMakeFiles/Silver.dir/src/game.cpp.o.d"
+  "libSilver.a"
+  "libSilver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Silver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include "game.hpp"
+
+using namespace std;
+int main() {
+
+
+
+}
