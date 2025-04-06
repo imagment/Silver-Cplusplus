8b3aeab30f57844156dc60ab13d37249e97b03ea
imagment <imagment@proton.me>
2025-04-06 04:58:28 -0400

    Fixed align function

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,12 @@ int main() {
 
 	c1.AddComponent<Camera>();
 
-	Actor actor("alert", "Hello World!");
+	Actor actor("alert", "Hello World!\na");
 
 	actor.GetComponent<Transform>()->position = Vector3Zero;
 
 	actor.GetComponent<Transform>()->scale = Vector3(1,1,1);
-
+	actor.GetComponent<SpriteRenderer>()->alignShapeTo(1);
 	actor.AddObject();
 
 	c1.GetComponent<Camera>()->RenderFrame();
